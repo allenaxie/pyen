0cab6716a32a1df568aaf17922730a4336fba40e
allenaxie <allenaxie1@gmail.com>
2022-08-07 12:11:36 -0700

    Create table for IPO Calendar.

--- pages/stocks/ipoCalendar/index.tsx
+++ pages/stocks/ipoCalendar/index.tsx
@@ -1,21 +1,87 @@
-const IPOCalendarPage = ({upcomingIPO}:any) => {
-    console.log(upcomingIPO);
+import { Table, Row, Col } from "antd";
+import classes from './ipoCalendar.module.scss';
+
+const IPOCalendarPage = ({ upcomingIPO }: any) => {
+
+    const columns = [
+        {
+            title: 'IPO Date',
+            dataIndex: 'ipoDate',
+            key: 'ipoDate',
+        },
+        {
+            title: 'Symbol',
+            dataIndex: 'symbol',
+            key: 'symbol',
+        },
+        {
+            title: 'Company Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Exchange',
+            dataIndex: 'exchange',
+            key: 'exchange',
+        },
+        {
+            title: 'Price Range',
+            dataIndex: 'ipoPriceLow',
+            key: 'priceRange',
+            render: (text: string, item: any) => (
+                <div>
+                    {text ?
+                        <div>
+                            ${text} - ${item.ipoPriceHigh}
+                        </div>
+                        :
+                        <div>N/A</div>
+                    }
+
+                </div>
+            )
+        },
+        {
+            title: 'Shares Offered',
+            dataIndex: 'sharesOffered',
+            key: 'sharesOffered',
+            render: (text: number) => <span>{text.toLocaleString()}</span>
+        },
+        {
+            title: 'Market Cap',
+            dataIndex: 'marketCap',
+            key: 'marketCap',
+            render: (text: number) => (
+                <div>
+                {text ?
+                    <div>
+                        ${text.toLocaleString()}
+                    </div>
+                    :
+                    <div>N/A</div>
+                }
+
+            </div>
+                )
+        }
+    ]
 
     return (
-        <>
-            {upcomingIPO.map((item:any) => (
-                <>
-                    {item.symbol}
-                    <br/>
-                </>
-            ))}
-        </>
+        <Row>
+            <Col
+                xs={{span: 24}}
+                className={classes.container}
+            >
+                <h1>IPO Calendar</h1>
+                <Table columns={columns} dataSource={upcomingIPO} />
+            </Col>
+        </Row>
     )
 }
 
 export default IPOCalendarPage;
 
-export async function getStaticProps (context:any) {
+export async function getStaticProps(context: any) {
     const res = await fetch('https://upcoming-ipo-calendar.p.rapidapi.com/ipo-calendar', {
         method: 'GET',
         headers: {
@@ -23,7 +89,7 @@ export async function getStaticProps (context:any) {
             'X-RapidAPI-Host': `${process.env.X_RapidAPI_Host}`,
         }
     })
-    const {data} = await res.json();
+    const { data } = await res.json();
 
     return {
         props: {
