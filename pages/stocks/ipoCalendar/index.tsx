830fb78b8ccba4e326677dfa271b1913a6b7771f
allenaxie <allenaxie1@gmail.com>
2022-08-06 22:53:10 -0700

    Fetch data from upcoming IPO API and render on page.

--- pages/stocks/ipoCalendar/index.tsx
+++ pages/stocks/ipoCalendar/index.tsx
@@ -1,9 +1,33 @@
-const IPOCalendarPage = () => {
+const IPOCalendarPage = ({upcomingIPO}:any) => {
+    console.log(upcomingIPO);
+
     return (
         <>
-            IPO Calendar
+            {upcomingIPO.map((item:any) => (
+                <>
+                    {item.symbol}
+                    <br/>
+                </>
+            ))}
         </>
     )
 }
 
 export default IPOCalendarPage;
+
+export async function getStaticProps (context:any) {
+    const res = await fetch('https://upcoming-ipo-calendar.p.rapidapi.com/ipo-calendar', {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': `${process.env.X_RapidAPI_Key}`,
+            'X-RapidAPI-Host': `${process.env.X_RapidAPI_Host}`,
+        }
+    })
+    const {data} = await res.json();
+
+    return {
+        props: {
+            upcomingIPO: data,
+        }
+    }
+}
