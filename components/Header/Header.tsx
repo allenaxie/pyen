f3a0a493a5c02df52e331ac10a43cf3734f6aa31
allenaxie <allenaxie1@gmail.com>
2022-08-06 20:34:14 -0700

    Update cta button functionality in header.

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,25 +1,43 @@
 import classes from './Header.module.scss';
-import {Row, Col} from 'antd';
+import { Row, Col } from 'antd';
 import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const Header = () => {
+
+    const { data: session } = useSession();
+
     return (
         <Row>
             <Col
-            xs={{span:24}}
-            lg={{span:12}}
-            className={classes.textContainer}
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
+                className={classes.textContainer}
             >
-                <div>Track all your assets <br/> in <span>one</span> place</div>
-                <button className={classes.ctaBtn}>Get started</button>
+                <div>Track all your assets <br /> in <span>one</span> place</div>
+                {session ?
+
+                    <button className={classes.ctaBtn}>
+                        <Link href='/profile'>
+                            My Profile
+                        </Link>
+                    </button>
+                    :
+                    <button className={classes.ctaBtn}>
+                        <Link href='/login'>
+                            Get started
+                        </Link>
+                    </button>
+                }
                 <p> #1 <span>personal</span> financial application</p>
             </Col>
             <Col
-                xs={{span:24}}
-                lg={{span:12}}
+                xs={{ span: 24 }}
+                lg={{ span: 12 }}
                 className={classes.imageContainer}
             >
-                <Image src="/Images/headerImg.png" width={600} height={300} alt="financeImg"/>
+                <Image src="/Images/headerImg.png" width={600} height={300} alt="financeImg" />
             </Col>
         </Row>
     )
